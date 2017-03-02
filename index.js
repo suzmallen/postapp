442e3cd0cbc1d56cbe15d19831ee44c5fe5c0044
suzmallen <suzandjosha@gmail.com>
2017-03-02 11:19:43 -0700

    Post Office app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,8 +14,74 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/calc', function(request, response) {
+    getEnteredValues(request,response,calculateRate);
+    response.render('pages/results');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+function getEnteredValues(request,response,calculateRate){
+    querystr= url.parse(request.url, true).query;
+    var type= querystr['type'];
+    var weight= parseFloat(querystr['weight']);
+     console.log(type);
+    console.log(weight);
+    calculateRate(response,type, weight);
+   
+}
 
+function calculateRate(response,type,weight){
+    var price = 0.00;
+    var round_weight = Math.round(weight);
+    switch(type){
+            
+        //Parcel
+        case "pl":
+        //if rounded package weight is <=4, price is $2.67 
+            //otherwise, base price = $2.67 + any rounded amount over 4 * .18 
+            if (round_weight<=4){
+                price=2.67;
+            }else{
+                price=2.67 + ((round_weight-4) * .18);
+            }
+                
+        break;
+            
+        //Postcard    
+        case "pc":
+        //ANY - 0.34    
+            price = .34;
+            break;
+            
+        //Large Envelope    
+        case "le":
+        //base price = .98 + any rounded amount over 1 * .21
+            price = .98 + ((round_weight-1) * .21);
+            break;
+            
+        //Stamped Letter    
+        case "sl":
+        //.49 + 21cents per ounce over 1 and anything over 3.5 ounces is 1.12
+            if(round_weight >= 4){
+                price = 1.12;
+            }else{
+                price = .49 + ((round_weight - 1) * .21)
+            }
+            break;
+            
+        //Metered Letter    
+        case "ml":
+        //3.5 or higher is 1.09 otherwise 46 + 21 cents per rounded ounce over 1
+            if(round_weight >=4){
+                price = 1.09;
+            }else{
+                price = .46 + ((round_weight - 1) * 21);
+            }
+            break;
+    }
+    var params = {price: price.toFixed(2), weight: weight, type: type };
+    response.render('pages/results',params);
+}
